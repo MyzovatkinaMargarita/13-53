79cc7b9994007e1df36e10e4284c75431ac5db1f
@Margarita52 <muzovatkinamargarita@gmail.com>
2026-04-23 02:24:51 +0300

    StudentStatsPage.tsx

---
+++ src/pages/student/StudentStatsPage.tsx
@@ -0,0 +1,11 @@
+// урок 18
+import StudentHeader from "../../components/student/StudentHeader";
+
+export default function StudentStatsPage() {
+  return (
+    <>
+      <StudentHeader title="Статистика" />
+      <div>Статистика (в разработке)</div>
+    </>
+  );
+}
