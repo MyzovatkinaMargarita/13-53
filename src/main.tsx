82ce11e5952e49b4055369065998defbb1ca1924
@Margarita52 <muzovatkinamargarita@gmail.com>
2026-04-23 02:41:20 +0300

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,3 +11,20 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <RouterProvider router={router} />
   </ThemeProvider>
 );
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./styles/theme";
+import "./styles/globals.css";
+import { router } from "./app/router";
+import { StoreProvider } from "./store/StoreProvider";
+import ModalHost from "./components/ui/ModalHost";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ThemeProvider theme={theme}>
+    <StoreProvider>
+      <RouterProvider router={router} />
+      <ModalHost />
+    </StoreProvider>
+  </ThemeProvider>
+);
