f5cfa72edaa6acc83fcc89ee39af8c65bb5b4a28
@Margarita52 <muzovatkinamargarita@gmail.com>
2026-04-23 02:06:02 +0300

    BackButton.tsx

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,7 @@
+// урок 15
+import { useNavigate } from "react-router-dom";
+
+export function BackButton() {
+  const navigate = useNavigate();
+  return <button onClick={() => navigate(-1)}>Назад</button>;
+}
