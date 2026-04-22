dee31a070a3d94f8f7c5b1b8f8348411fc420265
@Margarita52 <muzovatkinamargarita@gmail.com>
2026-04-23 01:53:32 +0300

    theme.ts

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+//урок 13, шаг 5
+export const theme = {
+  colors: {
+    primary: "#0E8E86",
+    accent: "#FF6A3D",
+    background: "#F7F9FA",
+    text: "#222222",
+  },
+  radius: {
+    sm: "4px",
+    md: "8px",
+    lg: "16px",
+  },
+};
+
+export type AppTheme = typeof theme;
