90e0e34b5009e446e4048e9418810404ecbbac03
@Margarita52 <muzovatkinamargarita@gmail.com>
2026-04-23 03:03:29 +0300

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,4 @@
-//урок 13, урок 14
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { ThemeProvider } from "@emotion/react";
-import { theme } from "./styles/theme";
-import "./styles/globals.css";
-import { router } from "./app/router";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <ThemeProvider theme={theme}>
-    <RouterProvider router={router} />
-  </ThemeProvider>
-);
+// уроки 13, 14, 41, 45
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "@emotion/react";
